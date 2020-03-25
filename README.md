b0d9459b5d9ae55f0f753af01417f6fa72d70990
Kiffie van Haash <kiffie@users.noreply.github.com>
2020-03-25 11:41:12 +0100

    create IHEX file with cargo-binutils (without PIC32 C toolchain)

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # pic32-rs-oled-demo
 Shows a moving Rust logo on an OLED display
 
+Building requires `xargo` and `cargo-binutils`, which can be installed with
+`cargo install`. A build can be done with
+
+    RUST_TARGET_PATH=$(pwd) xargo objcopy --bin oled-test --release -- -O ihex oled-test.hex
+
+See also the script `build.sh`
+
 ![Pic 32 OLED](https://raw.githubusercontent.com/kiffie/pic32-rs-oled-demo/master/doc/pic32-oled.jpg)
--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-#make -C native_lib
-RUST_TARGET_PATH=$(pwd) xargo build --target mipsel-none --release
+# Use xargo and cargo-binutils to generate an IHEX file to be flashed
+RUST_TARGET_PATH=$(pwd) \
+	xargo objcopy --bin oled-test --release -- -O ihex oled-test.hex
 
-pic32-bin2hex target/mipsel-none/release/oled-test
+
+# Use xargo to build an ELF file and the ChipKIT (or XC32) toolchain to convert
+# the ELF file to an IHEX file
+#RUST_TARGET_PATH=$(pwd) xargo build --target mipsel-none --release
+#pic32-bin2hex target/mipsel-none/release/oled-test
